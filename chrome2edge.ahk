09a5d22bcac1594e4213bc7cca7c49fbc6609b88
Alexis Rodriguez <aoeu512@gmail.com>
2024-03-20 17:42:07 -0500

    got working

--- chrome2edge.ahk
+++ chrome2edge.ahk
@@ -1,23 +1,3 @@
-
-; Define a hotkey (Ctrl + Shift + G) to trigger the script
-^+g::
-    ; Check if Microsoft Edge process exists
-    WinGet, edgeID, ID, ahk_exe msedge.exe
-    If (edgeID != "")
-    {
-        ; Activate Microsoft Edge window
-        WinActivate, ahk_pid %edgeID%
-        
-        ; Send Ctrl + T to open a new tab
-        Send, ^t
-        Sleep, 100
-        
-        ; Send the URL to navigate to (Google.com)
-        Send, https://www.google.com{Enter}
-    }
-    else
-    {
-        ; If Microsoft Edge process does not exist, show a message box
-        MsgBox, Microsoft Edge is not running.
-    }
+^#g:: ; Ctrl+Win+G hotkey
+Run, python.exe "%A_ScriptDir%\chrome2edge.py",, Hide
 return
--- chrome2edge.py
+++ chrome2edge.py
@@ -2,21 +2,47 @@ import pyautogui
 import pygetwindow as gw
 import time
 
-def switch_to_edge():
-    edge_windows = gw.getWindowsWithTitle("Edge")
-    if edge_windows:
-        # 如果有 Edge 窗口存在，则将焦点切换到第一个 Edge 窗口
-        edge_windows[0].activate()
-        print("已切换到 Microsoft Edge。")
-        time.sleep(1)  # 等待 Edge 窗口激活
-        pyautogui.hotkey('ctrl', 'l')
-        pyautogui.hotkey('ctrl', 'v')
-        print("地址栏已被突出显示并粘贴剪贴板内容。")
-    else:
-        print("未找到 Microsoft Edge 窗口。")
+import tkinter as tk
+import sys
+from tkinter import messagebox
 
-pyautogui.hotkey('ctrl', 'l')
-pyautogui.hotkey('ctrl', 'c')
+from io import StringIO
+
+sys.stderr = StringIO()
+root = tk.Tk()
+root.withdraw()  # 隐藏主窗口
+
+print = lambda x: messagebox.showinfo('信息', x)
+# 弹出警报框
+#messagebox.showinfo("警报", "这是一个警报框！")
+
+#root.mainloop()
+
+def switch_to_edge():    
+    edge_windows = gw.getWindowsWithTitle("Microsoft\u200b Edge")
+    
+    if edge_windows:        
+        edge_windows[0].activate()        
+        time.sleep(1)  # 等待 Edge 窗口激活
+        pyautogui.hotkey('ctrl', 't'); time.sleep(0.1)
+        pyautogui.hotkey('ctrl', 'l'); time.sleep(0.1)
+        pyautogui.hotkey('ctrl', 'v'); time.sleep(0.1)
+        pyautogui.hotkey('enter')
+        
+    else:
+        import subprocess
+        subprocess.run(command, shell=True)
+        time.sleep(1)
+        switch_to_edge()
+        return        
+
+chrome = [g for g in gw.getWindowsWithTitle('Google Chrome') if 'Translate' not in g.title]
+chrome[0].activate()
+time.sleep(1)
+pyautogui.hotkey('ctrl', 'l'); time.sleep(0.1)
+pyautogui.hotkey('ctrl', 'c'); time.sleep(0.1)
 
 # 调用函数以切换到 Edge 并执行相应操作
 switch_to_edge()
+
+root.mainloop()
